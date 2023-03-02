31389bcdb92990fe6c174f79e444978f168ab3f8
Walidmazit <wmazit@yahoo.fr>
2023-03-02 17:50:17 -0500

    logs

--- src/main/java/com/tutorialsninja/qa/base/CommonAPI.java
+++ src/main/java/com/tutorialsninja/qa/base/CommonAPI.java
@@ -1,5 +1,7 @@
 package com.tutorialsninja.qa.base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,6 +20,8 @@ public class CommonAPI {
     public WebDriver getDriver() {
         return driver;
     }
+    public static Logger log = LogManager.getLogger(CommonAPI.class.getName());
+
 
     WebDriver driver;
 
@@ -35,6 +39,7 @@ public class CommonAPI {
         driver.manage().window().maximize();
         driver.get(url);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        log.debug("url starting");
     }
 
     @AfterMethod
--- src/test/java/com/tutorialsninja/tests/TabletPageTest.java
+++ src/test/java/com/tutorialsninja/tests/TabletPageTest.java
@@ -3,9 +3,13 @@ package com.tutorialsninja.tests;
 import com.tutorialsninja.qa.base.CommonAPI;
 import com.tutorialsninja.qa.pages.LandingPage;
 import com.tutorialsninja.qa.pages.TabletPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
 public class TabletPageTest extends CommonAPI {
+    public static Logger log = LogManager.getLogger(TabletPageTest.class.getName());
+
 
 //@Test
 //public void clickTabletButton(){
@@ -25,6 +29,7 @@ public class TabletPageTest extends CommonAPI {
         TabletPage tabletPage = new TabletPage(getDriver());
 
         tabletPage.clickSamsungGalaxyTabText();
+        log.debug("clickSamsungGalaxyTabText");
 
     }
 
